f21b27f2537b1c25c5a94eb85e9d2f98d5033bd3
A L <ultimaterror@laposte.net>
2023-03-08 10:10:38 +0100

    Update Format : Object's Attributes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let quiz = [
     {
         question: "Comment s'appelle la peur d'être regardé par un canard ?",
-        input1: "L'hippophobie",
-        value1: "hippophobie",
-        input2: "L'anatidaephobie",
-        value2: "anatidaephobie",
-        input3: "La cynophobie",
-        value3: "cynophobie",
+        label1: "L'hippophobie",
+        inputValue1: "hippophobie",
+        label2: "L'anatidaephobie",
+        inputValue2: "anatidaephobie",
+        label3: "La cynophobie",
+        inputValue3: "cynophobie",
         goodAnswer: "anatidaephobie",
         infos: "On ne sait jamais si un canard est dans le coin et qu'il vous regarde du coin de l'oeil"
     }
