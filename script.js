c69bb3f50443a4c743b4725369141a3e5be20f7c
A L <ultimaterror@laposte.net>
2023-03-08 09:53:35 +0100

    First question / Question format

---
+++ script.js
@@ -0,0 +1,12 @@
+let quiz = [
+    {
+        question: "Comment s'appelle la peur d'être regardé par un canard ?",
+        input1: "L'hippophobie",
+        value1: "hippophobie",
+        input2: "L'anatidaephobie",
+        value2: "anatidaephobie",
+        input3: "La cynophobie",
+        value3: "cynophobie",
+        goodAnswer: "anatidaephobie"
+    }
+]
