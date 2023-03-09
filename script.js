699b2212bd4084a282f2a6a1425565469d9776bc
Wilder <delforceh@gmail.com>
2023-03-09 17:19:41 +0100

    ajout two restion

--- script.js
+++ script.js
@@ -11,5 +11,32 @@ let quiz = [
         inputValue4: "arachnophobie",
         goodAnswer: "anatidaephobie",
         infos: "On ne sait jamais si un canard est dans le coin et qu'il vous regarde du coin de l'oeil"
-    }
+    },
+    {
+        question: "Quel animal produit du lait de couleur rose ?",
+        label1: "Le flamant rose",
+        inputValue1: "flammentRose",
+        label2: "L'hippopotame",
+        inputValue2: "hippopotame",
+        label3: "Le cochon",
+        inputValue3: "cochon",
+        label4: "Le babouin bleu",
+        inputValue4: "babouinBleu",
+        goodAnswer: "hippopotame",
+        infos: " La raison ? L'hippopotame sécrète deux types d'acides, l'acide hipposudoric et l'acide norhipposudoric. Le premier est de couleur rouge vif, alors que le deuxième est de couleur orange vif"
+   
+    },
+    {
+    question: "Quelle espèce de requin n'existe pas ?",
+        label1: "Le requin à lunettes",
+        inputValue1: "requinLunettes",
+        label2: "Le requin bull-dog",
+        inputValue2: "requinBullDog",
+        label3: "Le requin citron",
+        inputValue3: "leRequinCitron",
+        label4: "Le requin lutin",
+        inputValue4: "requinLutin",
+        goodAnswer: "requinLunettes",
+        infos: "Savez-vous qu'il existe aussi le requin lézard, le requin taureau, le requin dormeur, le requin vache ou encore le squale à peau douce."
+}
 ]
