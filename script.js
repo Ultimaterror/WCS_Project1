23d9ccaddfe1076bd56e22ea10af3690bec49978
Brice <brice.verriere@epitech.eu>
2023-03-09 10:17:16 +0100

    add vrai & faux hidden divs with CSS

---
+++ script.js
@@ -0,0 +1,20 @@
+//Conditions if
+//ESSAI n°1
+let answer = quiz[goodAnswer];//vérifier comment est enregistré les réponses dans le JS
+let response = document.getElementById().value//trouver l'ID des boutons radio
+let score = 0;
+if(response == quiz[answer]){
+    //on modifie les couleurs des réponses (vert = OK, rouge = BAD)
+    //on affiche "Vrai" ou "Faux"
+    //on rend le bouton "question suivante" visible/cliquable
+    score++;
+}
+
+//DOM
+
+
+//Enregistrer le résultat
+
+
+//Renvoyer si juste ou faux
+
